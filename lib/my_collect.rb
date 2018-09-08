17b732a92809c7771ce99b7995290faf37aa9963
Shuo Wang <gken90@gmail.com>
2018-09-08 01:57:22 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,7 +9,7 @@ end
 
 
 my_collect(array) do |lang|
-  lang
+  lang.upcase
 end
 # You are writing a method that behaves just like the real #collect method.
 #  It should take in an argument of a collection, iterate over that collection using a while loop, and execute the code in the block you call it with for each element in the collection (use the yield keyword).
