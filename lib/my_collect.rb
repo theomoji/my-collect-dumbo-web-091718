a44d9f6d6072d350355c7c5c0a188168837abd82
Shuo Wang <gken90@gmail.com>
2018-09-08 01:55:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,7 +7,8 @@ def my_collect(array)
 end
 
 
-
+my_collect(array) do |element|
+  element
 # You are writing a method that behaves just like the real #collect method.
 #  It should take in an argument of a collection, iterate over that collection using a while loop, and execute the code in the block you call it with for each element in the collection (use the yield keyword).
 #  It should return the modified collection.
